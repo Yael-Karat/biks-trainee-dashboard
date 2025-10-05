409173a73d2e26aeb02f0ed987827e9a9353f8fd
Yael-Karat <yaelkarat01@gmail.com>
2025-10-05 11:37:23 +0300

    Enhance app bootstrap configuration with Material and Forms support

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-// src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app';
 import { appRoutes } from './app/app.routes';
+import { MaterialModule } from './app/material/material.module';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(appRoutes),
+    importProvidersFrom(MaterialModule, FormsModule)
   ]
 }).catch(err => console.error(err));
