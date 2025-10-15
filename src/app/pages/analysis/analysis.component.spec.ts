651d6397e88f49052f6ff9eefa030ea5286f598f
Yael-Karat <yaelkarat01@gmail.com>
2025-10-15 18:15:50 +0300

    Enhance AnalysisComponent unit tests: - Added mock DataService using jasmine SpyObj - Tested chart update logic to ensure charts initialize correctly - Migrated test setup to use standalone component imports

--- src/app/pages/analysis/analysis.component.spec.ts
+++ src/app/pages/analysis/analysis.component.spec.ts
@@ -1,16 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AnalysisComponent } from './analysis.component';
+import { DataService } from '../../services/data.service';
+import { of } from 'rxjs';
 
 describe('AnalysisComponent', () => {
   let component: AnalysisComponent;
   let fixture: ComponentFixture<AnalysisComponent>;
+  let mockDataService: jasmine.SpyObj<DataService>;
 
   beforeEach(async () => {
+    mockDataService = jasmine.createSpyObj('DataService', ['trainees$'], { trainees$: of([]) });
+
     await TestBed.configureTestingModule({
-      declarations: [AnalysisComponent]
-    })
-    .compileComponents();
+      imports: [AnalysisComponent],
+      providers: [{ provide: DataService, useValue: mockDataService }]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AnalysisComponent);
     component = fixture.componentInstance;
@@ -19,5 +23,10 @@ describe('AnalysisComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update charts without errors', () => {
+    component.updateCharts();
+    expect(component.charts.length).toBeGreaterThan(0);
   });
 });
